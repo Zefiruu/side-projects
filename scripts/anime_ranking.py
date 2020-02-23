3ef2b9f308606f5def7db244e385c58e5aba5db2
zefiru <zefiru@protonmail.com>
2020-02-23 22:51:12 +0000

    Improved formatting/readability

--- scripts/anime_ranking.py
+++ scripts/anime_ranking.py
@@ -1,22 +1,25 @@
 #! python3
-# anime_ranking.py - Webscrapes MyAnimeList.net's Anime ranking using bs4
+# anime_ranking.py - Webscrapes MyAnimeList.net's Anime ranking using bs4, then redirects User to link to watch online the anime of their choice.
 
 # Imports #
 import requests, re, pyperclip
 from bs4 import BeautifulSoup
 
 
-top_how_many = input('\nType the amount of animes you would like to see ranked - according to MyAnimeList.net\n Example: \n "100" would show the top 100 animes\n') # Amount to be displayed
+top_how_many = input('\nType the amount of animes you would like to see ranked - according to MyAnimeList.net\n Example: \n "100" would show the top 100 animes\n') 
 anime_list = [] # List of anime names to be selected by user
+
 print('\nRank | Score | Anime')
-# Loop fetching data from website
-for i in range(0,int(top_how_many),50):
-    url = 'https://myanimelist.net/topanime.php?limit=' + str(i) # MAL only shows in multiples of 50
+
+# Fetching data from website 
+for count in range(0,int(top_how_many),50): # Loop created to change the URL according to the amount of animes required
+    url = 'https://myanimelist.net/topanime.php?limit=' + str(count) 
     website = requests.get(url)
     content = website.content
     soup = BeautifulSoup(content,'lxml')
-    anime_names = soup.find_all('tr', {'class':'ranking-list'})
-    # Loop printing list in mulitples of 50
+    anime_https://www1.putlocker.digital/search/Hotaru%20no%20Hakanames = soup.find_all('tr', {'class':'ranking-list'})
+
+    # Printing each anime on page with Rank Position | Score | Anime Name
     for anime in anime_names:
         name_data = anime.find(class_='lazyload') # CSS selector for the anime name
         name = name_data.attrs['alt']
@@ -30,9 +33,11 @@ for i in range(0,int(top_how_many),50):
         print(anime_str)
         anime_list.append(name[7:])
 
-# Selection of anime to watch 
+# User input, picking the anime to watch 
 chosen_anime = input('\nNow type the number of the anime you would like to watch:\n')
 anime_to_watch = anime_list[int(chosen_anime)-1]
+
+# Redirecting based on User's input
 watch_link = 'https://www1.putlocker.digital/search/' + '%20'.join(anime_to_watch.split())
-print('\nYou chose: ' + anime_to_watch + '\n To watch the anime, paste this URL into a browser. For your convenience it has been saved to your clipboard. \n'+watch_link)
+print('\nYou chose: ' + anime_to_watch + '\n To watch the anime, paste this URL into your browser. For your convenience it has been saved to your clipboard. \n'+watch_link)
 pyperclip.copy(watch_link)
